e652ad66811462756c2ccb5e58d936b5bb45eb01
Poojaa <9050poojaa.p@gmail.com>
2026-01-10 01:56:10 +0530

    Fix MongoDB connection and Render port

--- server.js
+++ server.js
@@ -149,7 +149,6 @@ app.get("/watchlist/:userId", async (req, res) => {
 
 
 // MongoDB connection
-mongoose
  mongoose
   .connect(
     "mongodb+srv://9050poojaap_db_user:I1AY0c8A0Y57ETTv@streamflix-cluster.jzf2g4g.mongodb.net/streamflixDB?retryWrites=true&w=majority"
@@ -160,6 +159,9 @@ mongoose
   
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
